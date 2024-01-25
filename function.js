2076d21efafb0c5e6d35c1935b36e3366fae48d8
9534495797 <chaubeyankit927@gmail.com>
2024-01-25 10:24:31 +0530

    ok

---
+++ function.js
@@ -0,0 +1,16 @@
+// const multiply=function(a,b){
+//     return a*b;
+// };
+// const product=multiply(3,4);
+// console.log(product);
+// console.log(multiply(4,5));
+
+
+
+//arrow function
+// const subustract =(a,b)=>a-b;
+// console.log(subustract(6,4));
+
+
+const arith=(a,b)=>a+b;
+console.log(arith(6,4));
---
+++ shadowing.js
@@ -0,0 +1,14 @@
+//two var having sam name but scope is different
+// var shadowedvar="i am global.";
+// function ab(){
+// var shadowedvar="i am local";
+// console.log(shadowedvar);
+// }
+// console.log(shadowedvar);
+
+var x=20;
+if(x>=18){
+    console.log("eligible for vote in india.");
+}else{
+    console.log("not eligible for vote in india.");
+}
