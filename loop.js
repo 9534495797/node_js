1427d1343ff1aa52da634865727678be8d34a725
9534495797 <chaubeyankit927@gmail.com>
2024-01-29 05:44:57 +0530

    ok

---
+++ loop.js
@@ -0,0 +1,50 @@
+//for loop: execute code for specified number of time;
+// for(i=0;i<=5;i++){
+//     console.log(i);
+// }
+
+
+
+//while : repeat the code wjile the condition is true,it is also known as entry control
+
+// let x=0;
+// while(x<=5){
+//     console.log(x);
+//     x++;
+// }
+
+//do while loop //also known as exit controll
+// let i=0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=5);
+
+
+
+//for in loop>>iterates over the properties of an object.
+// let person={name:"ankit",age:"17",occupation:"devloper"};
+// for(let key in person){
+//     console.log(key+": "+person[key]);
+// }
+
+
+
+//seee understanding of js objects
+//it is collection of key-value pairs
+// let car={
+//     make:"toyota",
+//     model:"camry",
+//     year:2023,
+//     iselectric:false,
+//     start:function(){
+//         console.log("engine started!");
+//     }
+// };
+// console.log(car.make);
+// console.log(car.year);
+// console.log(car.start());
+// console.log(typeof(start));
+
+
+//remember operators precedense  and arrow function method in js
