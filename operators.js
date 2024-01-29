1d146143249f93cae97ba81fb4e7ab8e581a3537
9534495797 <chaubeyankit927@gmail.com>
2024-01-29 05:46:04 +0530

    ok

---
+++ operators.js
@@ -0,0 +1,40 @@
+//arithmetic 
+// let x=5;
+// let y=3;
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+
+//comparison
+
+// let x=3;
+// let y=3;
+// // console.log(x>y);
+// // console.log(x<y);
+// // console.log(x>=y);
+// // console.log(x<=y);
+// console.log(x===y);
+// console.log(x!==y);
+
+//transpiler will convert the newfeatures on old version platform according to that
+
+//logical operator
+// let p=true;
+// let q=false;
+// console.log(p&&q);
+// console.log(p||y);
+// console.log(!p);
+
+
+//assigment operator
+let num=5;
+num+=3;
+console.log(num);
+num-=3;
+console.log(num);
+num*=3;
+console.log(num);
+num/=3;
+console.log(num);
